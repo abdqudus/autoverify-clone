184a2f008761927bf8af73e9a88c312429c2bfe7
unknown <abdkhudusadeberyo@gmail.com>
2024-06-13 06:39:02 +0100

    m

--- src/component/LanguageSelect.jsx
+++ src/component/LanguageSelect.jsx
@@ -5,7 +5,7 @@ const LanguageSelect = () => {
     console.log(currentLanguage)
     return (
         <select
-            className="  sm:flex hidden justify-between gap-6 items-center"
+            className="  flex justify-between gap-6 items-center"
             onChange={onClickLanguageChange}
             value={currentLanguage}
         >
